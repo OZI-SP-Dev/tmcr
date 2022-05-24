69e0c3de9e32c61b954af03cfd918b1d7a2bed2c
RobertPorterfield <57455266+RobertPorterfield@users.noreply.github.com>
2022-05-24 15:57:46 -0400

    Updated leftnav button styles

--- src/components/AppLeftNav.tsx
+++ src/components/AppLeftNav.tsx
@@ -13,7 +13,7 @@ export const AppLeftNav = () => {
     <div className="sidebar-sticky bg-light">
       <h5 className="mt-5">Jump to Section</h5>
       <div className="d-grid gap-2 m-2">
-        {steps.map( (element, i) => <Button key={element} disabled={globalState.wizardMaxStep < i} variant={globalState.wizardStep===i ? "success" : globalState.wizardMaxStep >= i ? "primary" : "secondary"} className="text-start" onClick={e => dispatch({ type: 'GOTO_STEP', payload: { 'wizardStep': i } })}>{element}</Button>)}
+        {steps.map( (element, i) => <Button key={element} disabled={globalState.wizardMaxStep < i} variant={globalState.wizardStep===i ? "success" : globalState.wizardMaxStep >= i ? "primary" : "outline-secondary"} className="text-start" onClick={e => dispatch({ type: 'GOTO_STEP', payload: { 'wizardStep': i } })}>{element}</Button>)}
       </div>
     </div>
   );
