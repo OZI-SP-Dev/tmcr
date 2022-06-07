4454524a8b0205646842cf9375b5d941d15adcbc
RobertPorterfield <57455266+RobertPorterfield@users.noreply.github.com>
2022-06-07 12:01:23 -0400

    Added Table2

--- src/Steps/Steps.tsx
+++ src/Steps/Steps.tsx
@@ -2,11 +2,12 @@ import { Preface } from "./Preface";
 import { TOInfo } from "./TOInfo";
 import { TOProgramReqs } from "./TOProgramReqs";
 import { TMSSReqs } from "./TMSSReqs";
+import { Table2 } from "./Table2";
 import { FinalStep } from "./FinalStep";
 
 export const TMCRStepNames = ["Home", "TO Info", "TO Program Requirements", "TMSS Requirements", "IETM Functionaly Requirements", "Mandatory Requirements", "Configurable Requirements", "Delivery Requirements", "Spec/Std Interface Records (SIRS)", "Attachment 1", "Attachment 2", "Review/Print"];
 export const TMCRFinalStep = TMCRStepNames.length-1;
-const WizardSteps = [<Preface />, <TOInfo />, <TOProgramReqs />, <TMSSReqs />, , , , , , , , <FinalStep />];
+const WizardSteps = [<Preface />, <TOInfo />, <TOProgramReqs />, <TMSSReqs />, <Table2 />, , , , , , , <FinalStep />];
 // #TODO merge step names and wizard steps into a single object
 
 export interface ITMCRWizardSteps {
---
+++ src/Steps/Table2.tsx
@@ -0,0 +1,87 @@
+import { useContext } from "react";
+import { Form, Table } from "react-bootstrap";
+import { globalContext } from '../stateManagement/GlobalStore';
+
+export const Table2 = () => {
+  const { globalState, dispatch } = useContext(globalContext);
+  
+  const handleClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Bit hacky here using the any type, but it allows us to dynamically name the payload attributes
+    let payload:any = {};
+    payload[e.target.id] = e.target.checked;
+    
+    dispatch({ type: 'MERGE_OPTION', payload});
+  }
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Bit hacky here using the any type, but it allows us to dynamically name the payload attributes
+    let payload:any = {};
+    payload[e.target.id] = e.target.value;
+    
+    dispatch({ type: 'MERGE_OPTION', payload});
+  }
+  
+  return (
+    <div className="m-3">
+      <h1>TMCR Information</h1>
+      <h3>Table 2 Selections...</h3>
+      <br />
+      <h5>NOTES:</h5>
+      <ul className="text-start">
+        <li><h5>Check all applicable boxes</h5></li>
+        <li><h5>For existing manuals not being updated to latest specification requirements, enter “Same style and format” under the specification heading</h5></li>
+      </ul>
+
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>TO Number, Title, or Type of Manual or Chapter</th>
+            <th>Specification</th>
+            <th>Change</th>
+            <th>Revision</th>
+            <th>Supplement</th>
+            <th>Supplemental TM</th>
+            <th>Source Data</th>
+          </tr>
+        </thead>
+        <tbody>
+        {[...Array(20)].map( (element, i) => 
+            <tr key={"table2" + i}>
+              <td>{i+1}</td>
+              <td>
+              <Form.Control type="text"
+                aria-label={"Item " + i + " Title"}
+                value={globalState.wizardOptions["table2_title" + i]}
+                id={"table2_title" + i}
+                onChange={handleTextChange} />
+            </td>
+            <td>
+              <Form.Control type="text"
+                aria-label={"Item " + i + " Specification"}
+                value={globalState.wizardOptions["table2_specification" + i]}
+                id={"table2_specification" + i}
+                onChange={handleTextChange} /> 
+            </td>
+            <td>
+              <Form.Check type="switch" checked={globalState.wizardOptions["table2_change" + i]} id={"table2_change" + i} aria-label={"Item " + (i+1) + " Change"} onChange={handleClick} />
+            </td>
+            <td>
+              <Form.Check type="switch" checked={globalState.wizardOptions["table2_revision" + i]} id={"table2_revision" + i} aria-label={"Item " + (i+1) + " Revision"} onChange={handleClick} />
+            </td>
+            <td>
+              <Form.Check type="switch" checked={globalState.wizardOptions["table2_supplement" + i]} id={"table2_supplement" + i} aria-label={"Item " + (i+1) + " Supplement"} onChange={handleClick} />
+            </td>
+            <td>
+              <Form.Check type="switch" checked={globalState.wizardOptions["table2_supplemental_tm" + i]} id={"table2_supplemental_tm" + i} aria-label={"Item " + (i+1) + " Supplemental TM"} onChange={handleClick} />
+            </td>
+            <td>
+              <Form.Check type="switch" checked={globalState.wizardOptions["table2_source_data" + i]} id={"table2_source_data" + i} aria-label={"Item " + (i+1) + " Source Data"} onChange={handleClick} />
+            </td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
