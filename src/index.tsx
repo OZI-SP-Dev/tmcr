16078b83e62a5e6febf529bc93b22798003a35bd
RobertPorterfield <57455266+RobertPorterfield@users.noreply.github.com>
2022-07-06 11:51:35 -0400

    upgrade to react 18 rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStore } from './stateManagement/GlobalStore';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <link
       rel="stylesheet"
@@ -15,8 +18,7 @@ ReactDOM.render(
     />
     <GlobalStore><App /></GlobalStore>
     
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
