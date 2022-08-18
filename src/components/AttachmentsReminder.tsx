ef0037e828754be86d2da92eb9c2eb67a758bbe7
RobertPorterfield <57455266+RobertPorterfield@users.noreply.github.com>
2022-08-18 14:05:23 -0400

    Add TMSS Tailoring tool to toast

--- src/components/AttachmentsReminder.tsx
+++ src/components/AttachmentsReminder.tsx
@@ -15,12 +15,17 @@ export const AttachmentsReminder: FunctionComponent<IAttachmentsReminder> = (
     globalState.wizardOptions[0]?.tmcr_type === "S1000D" ||
     globalState.wizardOptions[1]?.tmcr_type === "S1000D";
 
+  const isLinear =
+    globalState.wizardOptions[0]?.tmcr_type === "Linear" ||
+    globalState.wizardOptions[1]?.tmcr_type === "Linear";
+
   return (
     <ToastContainer position="middle-center">
       <Toast show={props.show} onClose={props.handleClose} animation={true}>
         <Toast.Header>
           Remember to attach the SIRs
-          {isS1000D && " and S1000D Decision Point Tool"}.
+          {isS1000D && " and S1000D Decision Point Tool"}
+          {isLinear && " and TMSS Tailoring Tool"}.
         </Toast.Header>
       </Toast>
     </ToastContainer>
