84477f9e522497d0ce94481f1be891db71d85753
RobertPorterfield <57455266+RobertPorterfield@users.noreply.github.com>
2022-09-20 13:32:12 -0400

    Change from CDN to local copy of css

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { GlobalStore } from "./stateManagement/GlobalStore";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossOrigin="anonymous"
-    />
     <GlobalStore>
       <App />
     </GlobalStore>
